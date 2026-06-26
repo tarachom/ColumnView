2024e8fdf2479995db0f456e1a61c861632df703
Yura <tarachom@gmail.com>
2026-06-26 18:55:49 +0300

    up

--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
-﻿using Gtk;
-using System.Runtime.InteropServices;
-
-namespace Program;
+﻿namespace Program;
 
 class Program
 {
     private static void Main()
     {
-        SetMsysDirectory(@"D:\Debug\msys64\ucrt64\bin");
-
         var app = Application.New("ua.org.accounting.test", Gio.ApplicationFlags.FlagsNone);
         app.OnActivate += (_, _) =>
         {
@@ -34,7 +29,6 @@ class Program
             grid.AppendColumn(column);
 
             ScrolledWindow scroll = ScrolledWindow.New();
-            scroll.SetPolicy(PolicyType.Automatic, PolicyType.Automatic);
             scroll.SetChild(grid);
 
             window.Child = scroll;
@@ -43,24 +37,4 @@ class Program
 
         app.RunWithSynchronizationContext(null);
     }
-
-    public static void SetMsysDirectory(string path)
-    {
-        //Для Windows реєструється шлях до бібліотек Gtk
-        if (OperatingSystem.IsWindows())
-            if (Directory.Exists(path))
-            {
-                if (!NativeMethods.SetDllDirectory(path))
-                    Console.WriteLine("Warning: Failed to set DLL directory.");
-            }
-            else
-                Console.WriteLine($"Warning: MSYS2 path not found at {path}");
-    }
-}
-
-internal static partial class NativeMethods
-{
-    [LibraryImport("kernel32.dll", EntryPoint = "SetDllDirectoryW", StringMarshalling = StringMarshalling.Utf16, SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    public static partial bool SetDllDirectory(string lpPathName);
 }
